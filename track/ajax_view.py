56a73aada71f3816ebf7ed55480142f4716ca80f
Ali <aliasgar@outlook.com>
2013-03-04 00:53:14 +0530

    Update wsgi

--- track/ajax_view.py
+++ track/ajax_view.py
@@ -1,6 +1,10 @@
 from track.models import BusTravelLog
 from django.http import HttpResponse
+from django.shortcuts import render_to_response
 
 def last_trip(request, bus = '1'):
 	last = BusTravelLog.objects.get_last_trip(bus)
 	return HttpResponse("%s" % last)
+
+def current_trip(request, bus = '1'):
+	return render_to_response('current_coord.html', {'bus': bus})
--- track/urls.py
+++ track/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('track.ajax_view',
     url(r'^last_trip/(?P<bus>\d+)/$', 'last_trip'),
+    url(r'^current_trip/(?P<bus>\d+)/$', 'current_trip'),
 )
