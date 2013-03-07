d59c89c831fee32cf7f22380e3aed30b97ac320d
Ali <aliasgar@outlook.com>
2013-03-07 23:50:59 +0530

    Added new models and home page

--- cbustrack/settings.py
+++ cbustrack/settings.py
@@ -29,7 +29,7 @@ DATABASES = {
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en//ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ['localhost:8000', 'localhost', '.insigniadevs.com',]
+ALLOWED_HOSTS = ['localhost:8000', 'localhost', '.insigniadevs.com', '.pesseacm.org']
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
