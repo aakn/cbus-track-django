b1d175a3aba95883e8f034317882289a9f888d34
Ali <aliasgar@outlook.com>
2013-03-07 22:44:42 +0530

    Added new models and home page

--- static/js/load-current-info.js
+++ static/js/load-current-info.js
@@ -131,6 +131,9 @@ $(function(){
 	function update_table(lat,lon,time,moved,speed) {
 		$("#lat").html(lat);
 		$("#lon").html(lon);
+		var date = Date.parse(time);
+		time = date.toString("MMMM d, yyyy - hh:mm:ss tt");
+		time = time.replace(/ - 00:/, " - 12:");
 		$("#time").html(time);
 		$("#move").html(moved);
 		$("#speed").html(speed+" KMPH");
