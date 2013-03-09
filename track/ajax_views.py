21ecfdbc867f7d285baa6678edb062b0ccde0f04
Ali <aliasgar@outlook.com>
2013-03-09 18:01:07 +0530

    Fixed Last_trip data fetch

--- track/ajax_views.py
+++ track/ajax_views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render_to_response
 
 def last_trip(request, bus = '1', limit = '0'):
 	last = BusTravelLog.objects.get_last_trip(bus, int(limit))
-	return HttpResponse(last, content_type="application/text")
+	return HttpResponse(last)
 
 def current_trip(request, bus = '1'):
 	return render_to_response('current_trip.html', {'bus': bus})
