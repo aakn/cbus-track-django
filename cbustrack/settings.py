82541392a83e7b9d272a5ba1279e8e7d8362b858
Ali <aliasgar@outlook.com>
2013-03-10 13:11:33 +0530

    Added Caching

--- cbustrack/settings.py
+++ cbustrack/settings.py
@@ -10,7 +10,7 @@ ADMINS = (
 # Email setup
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'aliroks@gmail.com'
+EMAIL_HOST_USER = 'support@closerly.com'
 EMAIL_HOST_PASSWORD = 'booyah'
 EMAIL_PORT = 587
 
