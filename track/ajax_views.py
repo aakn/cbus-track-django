19f26d78c0675447fc004e5a0985c6b6a3611661
prashanth <prashanthjbabu@gmail.com>
2013-03-15 10:54:30 +0530

    daily

--- track/ajax_views.py
+++ track/ajax_views.py
@@ -20,4 +20,4 @@ def daily_req(request):
 	for item in timeset: 
 		time = item.time
 	time = time.date()
-	return render_to_response("The Time now is "+ time)
+	return render("The Time now is "+ time)
