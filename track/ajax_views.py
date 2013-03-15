7b68ae9df42eff237716757fa7c71b1576f9675a
prashanth <prashanthjbabu@gmail.com>
2013-03-15 10:51:44 +0530

    daily

--- track/ajax_views.py
+++ track/ajax_views.py
@@ -16,8 +16,8 @@ def daily_req(request):
 	from django.db.models import Count
 	count = DailyRequestCounter.objects.extra({'date' : "date(time)"}).values('date').annotate(counter=Count('id'))
 	#return render_to_response('dailyrequests/count.html', {'counter': count,})
-	 timeset = BusTravelLog.objects.filter(bus=1).order_by('-time')[:1]
+	timeset = BusTravelLog.objects.filter(bus=1).order_by('-time')[:1]
 	for item in timeset: 
 		time = item.time
 	time = time.date()
-	return render_to_response("The Time is "+ time)
+	return render_to_response("The Time now is "+ time)
