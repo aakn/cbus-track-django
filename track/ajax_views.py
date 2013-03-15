ba5b3d2d085266dac2af9aa4738866229fed3eaa
prashanth <prashanthjbabu@gmail.com>
2013-03-15 12:33:59 +0530

    removed ajax_view

--- track/ajax_views.py
+++ track/ajax_views.py
@@ -21,6 +21,6 @@ def daily_req(request):
 	#	time = item.time
 	#time = time.date()
 	#count =BusTravelLog.objects.extra({'date' : "date(time)"}).values('date').annotate(counter=Count('id'))
-	count =BusTravelLog.objects.filter(date__gt=date(time)+" 7:00:00")
+	count =BusTravelLog.objects.filter(time__gt=date(time)+" 7:00:00")
 	#return render_to_response("The Date now is "+ str(currdate))
 	return render_to_response('dailyrequests/count.html', {'counter': count,})
