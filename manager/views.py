0f0cd21e5bc66cb61131f9f9f722f22e276b1707
prashanth <prashanthjbabu@gmail.com>
2013-04-05 22:31:13 +0530

    creating manager interface

--- manager/views.py
+++ manager/views.py
@@ -9,7 +9,7 @@ import pusher, datetime
 
 def show_stats(request):
 	from django.db.models import Count
-	count = MapsAPIUsageCounter.objects.extra({'date' : "date(time)"}).values('date').annotate(counter=Count('id')).order_by('-id')[:5]
+	#count = MapsAPIUsageCounter.objects.extra({'date' : "date(time)"}).values('date').annotate(counter=Count('id')).order_by('-id')[:5]
 
 	#return render_to_response('manager/count.html', {'counter': count,})
 	log = []
@@ -23,29 +23,52 @@ def show_stats(request):
 		evening_lower_threshold = datetime.datetime(dateobj.year,dateobj.month,dateobj.day,15,00)
 		evening_upper_threshold = datetime.datetime(dateobj.year,dateobj.month,dateobj.day,20,00)	
 
-		morning_query = BusTravelLog.objects.extra({'date' : "date(time)"}).values('date').filter(time__gt=morning_lower_threshold).filter(time__lt=morning_upper_threshold).annotate(counter=Count('id'))
-		evening_query = BusTravelLog.objects.extra({'date' : "date(time)"}).values('date').filter(time__gt=evening_lower_threshold).filter(time__lt=evening_upper_threshold).annotate(counter=Count('id'))
+		morning_query_bus_1 = BusTravelLog.objects.extra({'date' : "date(time)"}).values('date').filter(time__gt=morning_lower_threshold).filter(time__lt=morning_upper_threshold).annotate(counter=Count('id')).filter(bus_id=1)
+		evening_query_bus_1 = BusTravelLog.objects.extra({'date' : "date(time)"}).values('date').filter(time__gt=evening_lower_threshold).filter(time__lt=evening_upper_threshold).annotate(counter=Count('id')).filter(bus_id=1)
+
+		morning_query_bus_2 = BusTravelLog.objects.extra({'date' : "date(time)"}).values('date').filter(time__gt=morning_lower_threshold).filter(time__lt=morning_upper_threshold).annotate(counter=Count('id')).filter(bus_id=2)
+		evening_query_bus_2 = BusTravelLog.objects.extra({'date' : "date(time)"}).values('date').filter(time__gt=evening_lower_threshold).filter(time__lt=evening_upper_threshold).annotate(counter=Count('id')).filter(bus_id=2)
+		
 		morning_maps_query = MapsAPIUsageCounter.objects.extra({'date' : "date(time)"}).values('date').filter(time__gt=morning_lower_threshold).filter(time__lt=morning_upper_threshold).annotate(counter=Count('id')).order_by('-id')[:5]
 		evening_maps_query = MapsAPIUsageCounter.objects.extra({'date' : "date(time)"}).values('date').filter(time__gt=evening_lower_threshold).filter(time__lt=evening_upper_threshold).annotate(counter=Count('id')).order_by('-id')[:5]
 
-		if(len(morning_query) > 0):
-			morning_count = morning_query[0]['counter']
+		if(len(morning_query_bus_1) > 0):
+			morning_count_bus_1 = morning_query_bus_1[0]['counter']
 		else:
-			morning_count = 0
+			morning_count_bus_1 = 0
 
-		if(len(evening_query) > 0):
-			evening_count = evening_query[0]["counter"]
+		if(len(evening_query_bus_1) > 0):
+			evening_count_bus_1 = evening_query_bus_1[0]["counter"]
 		else:
-			evening_count = 0
+			evening_count_bus_1 = 0
 
-		total_count = morning_count+ evening_count
+		total_count_bus_1 = morning_count_bus_1+ evening_count_bus_1
 		data = {
 			'date' : str(evening_upper_threshold.strftime("%B %d, %Y")),
-			'morning' : morning_count,
-			'evening' : evening_count,
-			'total' : total_count,
+			'morning' : morning_count_bus_1,
+			'evening' : evening_count_bus_1,
+			'total' : total_count_bus_1,
 		}
-		log.append(data)
+		log_bus_1.append(data)
+
+				if(len(morning_query_bus_2) > 0):
+			morning_count_bus_2 = morning_query_bus_2[0]['counter']
+		else:
+			morning_count_bus_2 = 0
+
+		if(len(evening_query_bus_2) > 0):
+			evening_count_bus_2 = evening_query_bus_2[0]["counter"]
+		else:
+			evening_count_bus_2 = 0
+
+		total_count_bus_2 = morning_count_bus_2+ evening_count_bus_2
+		data = {
+			'date' : str(evening_upper_threshold.strftime("%B %d, %Y")),
+			'morning' : morning_count_bus_2,
+			'evening' : evening_count_bus_2,
+			'total' : total_count_bus_2,
+		}
+		log_bus_2.append(data)
 
 		if(len(morning_maps_query) > 0):
 			morning_maps_count = morning_maps_query[0]['counter']
@@ -68,4 +91,4 @@ def show_stats(request):
 		dateobj = dateobj + delta
 	
 	#return render_to_response('track/daily_count.html', {'counter': log, 'request':request,})
-	return render_to_response('manager/count.html', {'counter': log, 'request':request,'counter2' : log_maps})
+	return render_to_response('manager/count.html', {'counter_bus_1': log_bus_1,'counter_bus_2': log_bus_2, 'request':request,'counter2' : log_maps})
