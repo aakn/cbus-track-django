39b0f8a8c6a89bfaf206cd0730fb7376e1704eba
prashanth <prashanthjbabu@gmail.com>
2013-04-05 21:08:37 +0530

    creating manager interface

--- manager/views.py
+++ manager/views.py
@@ -2,6 +2,10 @@ import json, urllib2
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from mapsapi.models import MapsAPIUsageCounter, MapsAddressCache# Create your views here.
+from track.models import Balance, BusTravelLog, RouteDetail
+from mapsapi.models import MapsAddressCache
+from track.convert_coordinates import convert
+import pusher, datetime
 
 def show_stats(request):
 	from django.db.models import Count
