8f65eeab82d0b424046a80db96b115a290f02272
prashanth <prashanthjbabu@gmail.com>
2013-04-05 22:00:51 +0530

    creating manager interface

--- manager/views.py
+++ manager/views.py
@@ -37,7 +37,7 @@ def show_stats(request):
 
 		total_count = morning_count+ evening_count
 		data = {
-			'date' : str(evening_upper_threshold.strftime("%A %B %d %Y")),
+			'date' : str(evening_upper_threshold.strftime("%B %d, %Y")),
 			'morning' : morning_count,
 			'evening' : evening_count,
 			'total' : total_count,
