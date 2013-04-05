37b2ce4f6e12013387f0176645be9bdc4467ed1f
prashanth <prashanthjbabu@gmail.com>
2013-04-05 20:52:27 +0530

    creating manager interface

--- manager/views.py
+++ manager/views.py
@@ -3,8 +3,8 @@ from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from mapsapi.models import MapsAPIUsageCounter, MapsAddressCache# Create your views here.
 
-def get_counter(request):
+def show_stats(request):
 	from django.db.models import Count
 	count = MapsAPIUsageCounter.objects.extra({'date' : "date(time)"}).values('date').annotate(counter=Count('id'))
 
-	return render_to_response('mapsapi/count.html', {'counter': count,})
+	return render_to_response('manager/count.html', {'counter': count,})
