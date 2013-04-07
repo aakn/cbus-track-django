77f43ab44da674992e82fb35d13ef00bcc3c293d
prashanth <prashanthjbabu@gmail.com>
2013-04-07 11:25:01 +0530

    new homepage with map of all buses

--- static/js/load-map.js
+++ static/js/load-map.js
@@ -112,7 +112,7 @@ $(function(){
 					var pos = new google.maps.LatLng(value.status[0],value.status[1]);
 					coord_array[i++] = pos;
 
-					alert("key="+key+"value="+value.status)
+					alert("key="+key+"value="+value.status[0])
 				});	
 				//update_table(lat,lon,time,"last-trip",speed);
 				console.log(coord_array); 
