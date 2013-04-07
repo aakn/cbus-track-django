96bb34c5e418192703f7b1bc256123c5188377fa
prashanth <prashanthjbabu@gmail.com>
2013-04-07 11:19:42 +0530

    new homepage with map of all buses

--- track/ajax_views.py
+++ track/ajax_views.py
@@ -36,7 +36,7 @@ def buses_status(request):
 	bus_status = []
 	for route in routes:
 		last = BusTravelLog.objects.get_last_trip(route, int(1))	
-		last2=simplejson.loads(last)
+		last2=simplejson.loads(last[0])
 		current_route = {
 			'id': route.id,
 			'status' : last2.lat
