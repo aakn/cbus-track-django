9264e929d3799bbb8f73d4c87034dd77148148a1
prashanth <prashanthjbabu@gmail.com>
2013-04-07 13:27:18 +0530

    new homepage with map of all buses

--- static/js/load-map.js
+++ static/js/load-map.js
@@ -229,11 +229,10 @@ $(function(){
 
 	window.update_route = function(new_id) {
 		bus_id = new_id;
-		alert("NEW ID ="+new_id);
 		if(bus_id==0)
 		{
-			$('#stats').hide();
-			$('#allstats').show();			
+			$('.stats').hide();
+			$('.allstats').show();			
 		}
 		else
 		{
