c1b03f77b9a4e35755cf1c44ee1ad04923a5736d
Ali <aliasgar@outlook.com>
2013-04-15 00:26:38 +0530

    Screwed up migrations :(

--- track/models.py
+++ track/models.py
@@ -38,7 +38,7 @@ class User(models.Model):
     notify = models.BooleanField(default=True)
     min_distance = models.CharField(max_length=10, default="2")
     min_time = models.CharField(max_length=10, default="5")
-    last_update_time = models.DateTimeField(auto_now=True, default='')
+    last_update_time = models.DateTimeField(auto_now=True, default='2012-01-01 01:00')
 
     def __unicode__(self):
         return "%s@%s" % (self.name, self.stop)
