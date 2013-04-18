4fff44e3b8d6ed9c204979e9de618931b1ec9a16
prashanth <prashanthjbabu@gmail.com>
2013-04-18 10:10:38 +0530

    get trip for any day

--- track/ajax_views.py
+++ track/ajax_views.py
@@ -80,9 +80,8 @@ def last_trip(request, bus = '1', limit = '0'):
 def trip(request, bus = '1', date='2013-04-18',morn_even='0'):
 	#last = BusTravelLog.objects.get_last_trip(bus, int(limit))
 	#return HttpResponse(last)
-	return HttpResponse("date is  "+date)
 	dateobj=datetime.datetime.strptime(date, "%Y-%m-%d").date()
-	dateobj = datetime.datetime.now()
+	#dateobj = datetime.datetime.now()
 
 	if(morn_even == '0'):
 		lower_threshold = datetime.datetime(dateobj.year,dateobj.month,dateobj.day,05,00)
