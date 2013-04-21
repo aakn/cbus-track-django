30384c65142f2a07063a7d12e055b705fe92c9a7
prashanth <prashanthjbabu@gmail.com>
2013-04-21 10:06:21 +0530

    adding distance for daily trip map

--- static/js/load-maps-for-management-trip.js
+++ static/js/load-maps-for-management-trip.js
@@ -124,9 +124,11 @@ function show_trip()
 	//displacement function for co ordinates
 	function computedisplacement(lat1,lon1,lat2,lon2) {
 		var theta=lon1-lon2;
+		console.log("theta="+theta);
 		var dist= Math.sin(deg2rad(lat1)) * Math.sin(deg2rad(lat2)) + Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * Math.cos(deg2rad(theta));
 		dist = Math.acos(dist);
 		dist = rad2deg(dist);
+		console.log("dist="+dist);
 		dist = dist * 60 * 1.1515;
 		dist = dist * 1.609344;
 		return dist;
