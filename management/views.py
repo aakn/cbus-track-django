5aa116bedf9bc420454100c0fadb1e6c020342d0
prashanth <prashanthjbabu@gmail.com>
2013-04-23 19:01:57 +0530

    management daily travel stats

--- management/views.py
+++ management/views.py
@@ -86,5 +86,5 @@ def daily_stats(request):
 
 		}
 		log_per_bus.append(final_log)
-		return HttpResponse("<br/>".join(temp))
+		return HttpResponse(join(temp))
 #	return render_to_response('management/daily_stats.html', {'buslog': log_per_bus, 'request':request,})
